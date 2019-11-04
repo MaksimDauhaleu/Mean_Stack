394653848e84fff7ddf15a05c3c5570322ec28c5
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-11-04 14:35:25 -0800

    express

---
+++ mean_intro/Express/ejsDogsCars/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();  
+
+
+
+//Static File SetUp
+app.use(express.static(__dirname + "/static"));
+
+
+
+// Templates Engine SetUp
+app.set('view engine', 'ejs');
+
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.render('index.html');
+});
+
+
+
+app.get("/dogs", (req, res) => { 
+    res.render('dogs');
+});
+
+
+
+app.get("/cars", (req, res) => { 
+    res.render('dogs');
+});
+
+
+
+// Listening port
+app.listen(8000, () => console.log("listening on port 8000"));
+
