ba512ce0acf769865f9b9196f102d533cdb40cd9
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-11-05 20:20:07 -0800

    d

---
+++ mean_projects/quotingDojo/base.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();  
+const mongoose = require('mongoose');
+const flash = require('express-flash');
+
+
+//File SetUp
+app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/name_of_your_DB', {useNewUrlParser: true});
+
+
+
+// Templates Engine SetUp
+app.set('view engine', 'ejs');
+
+
+
+
+// Database
+const QuoteSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 6},
+    notName: { type: String},
+   })
+   const Quote = mongoose.model('Quote', QuoteSchema);
+
+
+
+
+
+//Routes
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+app.post('/quotes',(req,res)=>{
+    const quote = new Quote();
+    quote.name = req.body.name;
+    quote.notName = req.body.notName;
+    quote.save()
+        .then(newUserData => {
+            console.log('user created: ', newUserData);
+            res.redirect('/quotes')
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect('/')
+        });
+    })
+
+
+app.get('/quotes',(req,res)=>{
+    Quote.find()
+        .then(quotes =>{
+            res.render('quotes',{quotes})
+        })
+        .catch(err => res.json(err))
+})
+
+
+
+// Listening port
+app.listen(8000, () => console.log("listening on port 8000"))
